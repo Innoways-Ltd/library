3f90807bc0aec24bd723e8b5cf0e68c84e5e7e8a
Eric Lee <ericlee@Erics-MacBook-Air.local>
2023-07-13 18:12:37 +0800

    Added console

--- common.js
+++ common.js
@@ -1,5 +1,6 @@
 (function () {
   window.addEventListener('message', ({ data }) => {
+    console.log("in frame", data)
     if (data?.type && data.type == "handleIframeNavigation") {
       const where = parseInt(data?.where)
       window.history.go(where)
